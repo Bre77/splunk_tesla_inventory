5c6cc382e52638f7a17fe9a12c21c3adff0738ca
Brett <brett.whynot@gmail.com>
2024-09-12 11:33:18 +1000

    Fix build

--- .build.sh
+++ .build.sh
@@ -3,6 +3,6 @@ cd "${0%/*}"
 OUTPUT="${1:-tesla_inventory.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
+chmod -R u=rwx,go= bin/*
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite tesla_inventory
